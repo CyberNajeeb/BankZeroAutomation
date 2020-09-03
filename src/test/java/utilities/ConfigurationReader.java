8624048e20a67d1df4a74c8a439985af141fd63c
Mohammed Sohrabi <najeebsohrabi@gmail.com>
2020-09-03 00:55:45 -0700

    commit

---
+++ src/test/java/utilities/ConfigurationReader.java
@@ -0,0 +1,39 @@
+package utilities;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class ConfigurationReader {
+    private static Properties configFile;
+
+    static {
+        try {
+            //location of properties file
+            //File.separator makes the code OS-independent.
+            String path = System.getProperty("user.dir") + File.separator + "configuration.properties";
+            //get that file as a stream
+            FileInputStream input = new FileInputStream(path);
+            //create object of Properties class
+            configFile = new Properties();
+            //load properties file into Properties object
+            configFile.load(input);
+            //close the input
+            input.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to load properties file!");
+        }
+
+    }
+
+    /**
+     * This method returns property value from configuration.properties file
+     *
+     * @param keyName property name
+     * @return property value
+     */
+    public static String getPropertyFromPropertiesFile(String keyName) {
+        return configFile.getProperty(keyName);
+    }
+}
---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,131 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Driver {
+
+    //same for everyone
+    private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    //so no one can create object of Driver class
+    //everyone should call static getter method instead
+    private Driver() {
+
+    }
+
+    /**
+     * synchronized makes method thread safe. It ensures that only 1 thread can use it at the time.
+     * <p>
+     * Thread safety reduces performance but it makes everything safe.
+     *
+     * @return
+     */
+    public synchronized static WebDriver getDriver() {
+        //if webdriver object doesn't exist
+        //create it
+        if (driver.get() == null) {
+            //specify browser type in configuration.properties file
+            String browser = ConfigurationReader.getPropertyFromPropertiesFile("browser").toLowerCase();
+            switch (browser) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.addArguments("--start-maximized");
+                    driver.set(new ChromeDriver(chromeOptions));
+                    break;
+                case "chromeheadless":
+                    //to run chrome without interface (headless mode)
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions options = new ChromeOptions();
+                    options.setHeadless(true);
+                    driver.set(new ChromeDriver(options));
+                    break;
+                case "chrome-remote":
+                    try {
+                        URL url = new URL("http://localhost:4444/wd/hub");
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setCapability("platform", Platform.ANY);
+                        desiredCapabilities.setBrowserName(BrowserType.CHROME);
+                        driver.set(new RemoteWebDriver(url, desiredCapabilities));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver.set(new FirefoxDriver());
+                    break;
+                default:
+                    throw new RuntimeException("Wrong browser name!");
+                case "firefox-remote":
+                    try {
+                        URL url = new URL("http://54.167.95.36:4444/wd/hub");
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setBrowserName(BrowserType.FIREFOX);
+                        desiredCapabilities.setPlatform(Platform.ANY);
+
+                        driver.set(new RemoteWebDriver(url, desiredCapabilities));
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+        }
+        return driver.get();
+    }
+
+    /**
+     * synchronized makes method thread safe. It ensures that only 1 thread can use it at the time.
+     * <p>
+     * Thread safety reduces performance but it makes everything safe.
+     *
+     * @return
+     */
+    public synchronized static WebDriver getDriver(String browser) {
+        //if webdriver object doesn't exist
+        //create it
+        if (driver.get() == null) {
+            //specify browser type in configuration.properties file
+            switch (browser) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.addArguments("--start-maximized");
+                    driver.set(new ChromeDriver(chromeOptions));
+                    break;
+                case "chromeheadless":
+                    //to run chrome without interface (headless mode)
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions options = new ChromeOptions();
+                    options.setHeadless(true);
+                    driver.set(new ChromeDriver(options));
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver.set(new FirefoxDriver());
+                    break;
+                default:
+                    throw new RuntimeException("Wrong browser name!");
+            }
+        }
+        return driver.get();
+    }
+
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.get().quit();
+            driver.remove();
+        }
+    }
+}
